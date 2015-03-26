55d1134f1c540e5ede4924b6c270aa0a6f57136c
Rolands Atvars <rolands.atvars@gmail.com>
2015-03-26 23:17:58 +0200

    Add HTML tag builder

---
+++ src/HtmlTagBuilder.php
@@ -0,0 +1,52 @@
+<?php
+namespace SDS\Dytomate;
+
+class HtmlTagBuilder
+{
+    public function build($tag, $content, array $attributes)
+    {
+        $html = "<{$tag} {$this->buildAttributeString($attributes)}";
+
+        if ($this->isSelfClosing($tag)) {
+            $html .= " />";
+        } else {
+            $html .= ">{$content}</{$tag}>";
+        }
+
+        return $html;
+    }
+
+    protected function isSelfClosing($tag)
+    {
+        return in_array($tag, [
+            "area",
+            "base",
+            "br",
+            "col",
+            "command",
+            "embed",
+            "hr",
+            "img",
+            "input",
+            "keygen",
+            "link",
+            "meta",
+            "param",
+            "source",
+            "track",
+            "wbr"
+        ]);
+    }
+
+    protected function buildAttributeString(array $attributes)
+    {
+        $html = [];
+
+        foreach ($attributes as $name => $value) {
+            $value = htmlspecialchars($value, ENT_COMPAT | ENT_HTML5, "UTF-8", false);
+            $html[] = "{$name}=\"{$value}\"";
+        }
+
+        return implode(" ", $html);
+    }
+}
