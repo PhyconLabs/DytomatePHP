da8ff3ee17257aef999aa5070da4b01eaeeca44e
Rolands Atvars <rolands.atvars@gmail.com>
2015-04-08 01:26:42 +0300

    Add default data array implementation

---
+++ src/ArrayDefaultData.php
@@ -0,0 +1,44 @@
+<?php
+namespace SDS\Dytomate;
+
+class ArrayDefaultData implements DefaultData
+{
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public function get($key)
+    {
+        return $this->has($key) ? $this->data[$key]["value"] : null;
+    }
+
+    public function getAttribute($key, $attribute)
+    {
+        return $this->hasAttribute($key, $attribute) ? $this->data[$key]["attributes"][$attribute] : null;
+    }
+
+    public function set($key, $value, array $attributes = [])
+    {
+        if (!isset($this->data[$key])) {
+            $this->data[$key] = [ "value" => null, "attributes" => [] ];
+        }
+
+        $this->data[$key]["value"] = $value;
+        $this->data[$key]["attributes"] = $attributes;
+
+        return $this;
+    }
+
+    public function has($key)
+    {
+        return isset($this->data[$key], $this->data[$key]["value"]);
+    }
+
+    public function hasAttribute($key, $attribute)
+    {
+        return isset($this->data[$key], $this->data[$key]["attributes"][$attribute]);
+    }
+}
